5795fa7d0309798b1d054e582028012198e1f652
Chris <csthompson521@gmail.com>
2019-02-10 12:04:01 -0800

    Add existing file

--- DBcm.py
+++ DBcm.py
@@ -1,6 +1,10 @@
 import mysql.connector
 
-class ConnectionError(Excception):
+class ConnectionError(Exception):
+    pass
+class CredentialsError(Exception):
+    pass
+class SQLError(Exception):
     pass
 
 class UseDatabase:
@@ -15,10 +19,15 @@ class UseDatabase:
             return self.cursor
         except mysql.connector.errors.InterfaceError as err:
                  raise ConnectionError(err)
+        except mysql.connector.errors.ProgrammingError as err:
+            raise CredentialsError(err)
                 
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
         self.conn.commit()
         self.cursor.close()
         self.conn.close()
-
+        if exc_type is mysql.connector.errors.ProgrammingError:
+            raise SQLError(exc_value)
+        elif exc_type:
+            raise exc_type(exc_value)
     
--- vsearch4web.py
+++ vsearch4web.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, escape, session
 from vsearch import search4letters
 from vsearchchecker import check_logged_in
-from DBcm import UseDatabase, ConnectionError
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 
 app = Flask(__name__)
 
@@ -12,17 +12,27 @@ app.config['dbconfig'] = {'host': '127.0.0.1',
                           'database': 'vsearchlogDB', }
 def log_request(req: 'flask_request', res: str) -> None:
     """Log details of the web request and the results."""
-    with UseDatabase(app.config['dbconfig']) as cursor: 
-        _SQL = """insert into log
-                  (phrase, letters, ip, browser_string, results)
-                  values
-                  (%s, %s, %s, %s, %s)"""
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor: 
+            _SQL = """insert into log
+                      (phrase, letters, ip, browser_string, results)
+                      values
+                      (%s, %s, %s, %s, %s)"""
 
-        cursor.execute(_SQL, (req.form['phrase'],
-                              req.form['letters'],
-                              req.remote_addr,
-                              req.user_agent.browser,
-                              res, ))
+            cursor.execute(_SQL, (req.form['phrase'],
+                                  req.form['letters'],
+                                  req.remote_addr,
+                                  req.user_agent.browser,
+                                  res, ))
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:', str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+    return 'Error'
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -57,17 +67,21 @@ def view_the_log() -> 'html':
     try:
         with UseDatabase(app.config['dbconfig']) as cursor:
             _SQL = """select phrase, letters, ip, browser_string, results
-                  from log"""
+                  from og"""
             cursor.execute(_SQL)
             contents = cursor.fetchall()
-        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        titles = ('phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
         return render_template('viewlog.html',
                                the_title='View Log',
                                the_row_titles=titles,
                                the_data=contents,)
     except ConnectionError as err:
         print('Is your database switched on? Error:', str(err))
-    except Excception as err:
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:', str(err))
+    except Exception as err:
         print('Something went wrong:', str(err))
     return 'Error'
 
